25ea8204eb66cc9af804b2167ef3783d88b47700
Vinicius Mendonca <viniciusmendonca@aridesa.com.br>
2016-11-25 17:13:40 -0300

    Adesão da alteração do produto (Incompleta)

---
+++ produto-altera-formulario.php
@@ -0,0 +1,60 @@
+<?php
+//Inclui o cabeçalho
+include("cabecalho.php");
+//Conexão com o banco
+include("conecta.php");
+//Funções da tabela de categorias
+include("banco-categoria.php");
+
+$categorias = listaCategorias($conexao);
+
+?>
+
+		<h1>Formulário de produto</h1>
+
+	<!--Adiciona oproduto pelo metodo POST-->
+	<form action="adiciona-produto.php" method="post">
+		<table class="table">
+		<tr>
+			<td>Nome:</td>
+			<td><input class="form-control" type="text" name="nome"><br /></td>
+		</tr>
+		<tr>
+			<td>Preço:</td>
+			<td><input class="form-control" type="number" name="preco"><br /></td>
+		</tr>
+		<tr>
+			<td>Descricao</td>
+			<td><textarea class="form-control" name="descricao"></textarea></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="checkbox" name="usado" value="true">Usado
+		</tr>
+		<tr>
+			<td>Categoria</td>
+			<td>
+				<!--Lista cada categoria cadastrada no banco como radio button-->
+				<?php // foreach($categorias as $categoria) : ?>
+					 <!-- <input type="radio" name="categoria_id"
+						value="<?=$categoria['id']?>">
+						<?=$categoria['nome']?><br/> --> 
+				<?php // endforeach ?>
+				<!--Selecionar no combobox-->
+				<select name="categoria_id" class="form-control">
+					<?php foreach($categorias as $categoria) : ?>
+						<option value="<?=$categoria['id']?>">
+							<?=$categoria['nome']?>
+						</option>
+					<?php endforeach ?>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td><input class="btn btn-primary" type="submit" name="Cadastrar"></td>
+		</tr>	
+
+	</form>
+
+<!--Inclui o rodape-->
+<?php include("rodape.php") ?>
--- produto-lista.php
+++ produto-lista.php
@@ -24,6 +24,7 @@ include("banco-produto.php"); ?>
 			<td><?= $produto['preco']?></td>
 			<td><?= substr($produto['descricao'], 0, 40)?></td>
 			<td><?=$produto['categoria_nome']?></td>
+			<td><a href="produto-altera-formulario.php?id<?=$produto['id']?>" class="btn btn-primary">alterar</a></td>
 			<td>
 			<form action="remove-produto.php" method="post">
 			<input type="hidden" name="id" value="<?=$produto['id']?>">
